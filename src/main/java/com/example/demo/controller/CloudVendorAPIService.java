acf17ed6845b2ec86428caf18f765068b9073603
shivanishrivas891@ <shivanishrivas891@gmail.com>
2024-07-29 23:50:17 +0530

    created rest controller demo

---
+++ src/main/java/com/example/demo/controller/CloudVendorAPIService.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorID)
+    {
+         return new CloudVendor("C1","Vendor1",
+             "Address1", "XXX");
+    }
+            @PostMapping
+            public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+            {
+                this.cloudVendor = cloudVendor;
+                return "Cloud Vendor Created Successfully";
+
+            }
+
+//        @PostMapping
+//        public String UpdateCloudVendorDetails (@RequestBody CloudVendor cloudVendor)
+//        {
+//            this.cloudVendor = cloudVendor;
+//            return "Cloud Vendor Updated Successfully";
+//
+//        }
+    }
+
+
+
---
+++ src/main/java/com/example/demo/model/CloudVendor.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+public class CloudVendor {
+
+    private String vendorId;
+    private String vendorName;
+    private String VendorAddress;
+    private String VendorPhoneNumber;
+
+
+    public CloudVendor() {
+    }
+
+    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
+        this.vendorId = vendorId;
+        this.vendorName = vendorName;
+        VendorAddress = vendorAddress;
+        VendorPhoneNumber = vendorPhoneNumber;
+    }
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return VendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        VendorAddress = vendorAddress;
+    }
+
+    public String getVendorPhoneNumber() {
+        return VendorPhoneNumber;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        VendorPhoneNumber = vendorPhoneNumber;
+    }
+
+
+
+}
